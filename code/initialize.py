d4c835c51e278c74b75d3d849ddd0a108cfa04c2
apetri <apetri@phys.columbia.edu>
2015-05-22 11:48:56 -0400

    compute lens redshifts for each model

--- code/initialize.py
+++ code/initialize.py
@@ -12,6 +12,8 @@ from lenstools.simulations.camb import CAMBSettings
 from lenstools.simulations.gadget2 import Gadget2Settings
 
 import numpy as np
+import astropy.units as u
+from astropy.cosmology import z_at_value
 
 git = LocalGit()
 
@@ -21,7 +23,9 @@ ngenic = NGenICSettings()
 gadget2 = Gadget2Settings()
 
 zmax = 3.1
-nlenses = 60
+box_size_Mpc_over_h = 260.0
+nside = 512
+lens_thickness_Mpc = 120.0
 
 #NGenIC
 ngenic.GlassFile = lenstools.data("dummy_glass_little_endian.dat")
@@ -57,7 +61,7 @@ if "--tree" in sys.argv:
 		#Lay down directory tree
 		cosmo = LensToolsCosmology(Om0=Om,Ode0=1-Om,w0=w,sigma8=si8)
 		model = batch.newModel(cosmo,parameters=["Om","Ol","w","si"])
-		collection = model.newCollection(box_size=260.0*model.Mpc_over_h,nside=512)
+		collection = model.newCollection(box_size=box_size_Mpc_over_h*model.Mpc_over_h,nside=nside)
 		r = collection.newRealization(seed)
 
 
@@ -69,7 +73,7 @@ if "--camb" in sys.argv:
 		collection.writeCAMB(z=np.array([0.0]),settings=camb)
 
 
-if "--pfiles" in sys.argv:
+if ("--lenses" in sys.argv) or ("--pfiles" in sys.argv):
 
 	#Compute comoving distance to maximum redshift for each model
 	d = list()
@@ -79,15 +83,29 @@ if "--pfiles" in sys.argv:
 	#Compute lens spacings
 	d = np.array([dv.value for dv in d]) * d[0].unit
 
+	#We want to make sure there are lenses up to the maximum of these distances
+	lens_distances = np.arange(lens_thickness_Mpc,d.max().to(u.Mpc).value + lens_thickness_Mpc,lens_thickness_Mpc) * u.Mpc
+
 	for model in batch.available:
 
-		collection = model.collections[0]
-		r = collection.realizations[0]
-
-		#ngenic parameter file
-		r.writeNGenIC(ngenic)
-
 		#Compute the redshifts of the Gadget snapshots
+		z = np.zeros_like(lens_distances.value)
+		for n,dlens in enumerate(lens_distances):
+			z[n] = z_at_value(model.cosmology.comoving_distance,dlens)
 
-		#Gadget parameter file
-		r.writeGadget2(gadget2)
+		#Assgn values to gadget settings
+		gadget2.OutputScaleFactor = np.sort(1/(1+z))
+
+		if "--pfiles" in sys.argv:
+		
+			collection = model.collections[0]
+			r = collection.realizations[0]
+
+			#ngenic parameter file
+			r.writeNGenIC(ngenic)
+
+			#Gadget parameter file
+			r.writeGadget2(gadget2)
+
+		else:
+			print(gadget2.OutputScaleFactor)
