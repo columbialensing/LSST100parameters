da38713419742155c54aed658c82fbb5bfba3b31
apetri <apetri@phys.columbia.edu>
2015-08-04 18:56:03 -0400

    test video.py

--- code/video.py
+++ code/video.py
@@ -6,6 +6,9 @@ sys.modules["mpi4py"] = None
 from lenstools.pipeline.simulation import SimulationBatch
 from lenstools.simulations import PotentialPlane
 
+#Get handle on the plane set
 batch = SimulationBatch.current()
 model = batch.available[0]
-print(model)
+plane_set = model.collections[0].realizations[0].planesets[0]
+
+print(plane_set)
