00695d2afd631aeaa523b9cd0256a51ad28c56ac
apetri <apetri@phys.columbia.edu>
2015-10-03 21:13:22 -0400

    compute constraints projecting on (vOm,vw)

--- code/cross_power_constraints.py
+++ code/cross_power_constraints.py
@@ -5,11 +5,12 @@ import numpy as np
 from lenstools.statistics.ensemble import Ensemble
 from lenstools.statistics.constraints import Emulator
 from library.featureDB import FeatureDatabase
+from library.algorithms import Manifold
 import library.driver_mpi as driver
 
 #Score parameters
 multipole_names = ["l{0}".format(n) for n in range(1,51)]
-score_parameters = Ensemble.meshgrid({"Om":np.linspace(0.2,0.4,100),"w":np.linspace(-1.1,-0.9,100)})
+score_parameters = Ensemble.meshgrid({"Om":np.linspace(0.24,0.27,100),"w":np.linspace(-1.1,-1.0,100)})
 score_parameters["sigma8"] = 0.8
 
 #Specifications
@@ -26,13 +27,24 @@ with FeatureDatabase("../models/cross_spectra_fiducial.sqlite") as db:
 		specs[feature_name] = dict()
 
 		#Add the emulator
-		specs[feature_name]["emulator"] = Emulator.read("../models/emulator_power_cross_{0}{0}.pkl".format(n))
+		emulator = Emulator.read("../models/emulator_power_cross_{0}{0}.pkl".format(n))
+		manifold = Manifold(emulator)
+		curves = manifold.draw_tangents()
+		v = (curves['Om'].iloc[1],curves['w'].iloc[1])
+
+		#Refeaturize the emulator
+		specs[feature_name]["emulator"] = emulator.refeaturize(lambda f:manifold.pca.transform(f).project(v,names=['vOm','vw']),method="apply_whole")
+		specs[feature_name]["emulator"].to_pickle("../models/emulator_power_cross_projected_{0}{0}.pkl".format(n)) 
+
 
 		#Get the data and compute the covariance matrix
 		pow = db.query("SELECT {0} FROM features_fiducial WHERE b1={1} AND b2={1}".format(",".join(multipole_names),n))
 		pow.add_name(feature_name)
+		pow = manifold.pca.transform(pow).project(v,names=['vOm','vw'])
+		pow.add_name(feature_name)
+
 		specs[feature_name]["data"] = pow.head(1600).mean()
-		specs[feature_name]["data_covariance"] = pow.cov() 
+		specs[feature_name]["data_covariance"] = pow.cov() / 1600
 
 #Execute
-driver.chi2database(db_name="../models/power_cross_scores.sqlite",parameters=score_parameters,specs=specs,pool=None,nchunks=None)
+driver.chi2database(db_name="../models/power_cross_scores_projected_scaled.sqlite",parameters=score_parameters,specs=specs,pool=None,nchunks=None)
