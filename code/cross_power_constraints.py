85a550f3ae742138d532c855028f203163e81134
apetri <apetri@phys.columbia.edu>
2015-10-05 10:30:55 -0400

    normal and projected constraints

--- code/cross_power_constraints.py
+++ code/cross_power_constraints.py
@@ -17,34 +17,41 @@ score_parameters["sigma8"] = 0.8
 specs = dict()
 
 #Open the fiducial cosmology database for data and covariances
-with FeatureDatabase("../models/cross_spectra_fiducial.sqlite") as db:
+with FeatureDatabase("../data/features/cross_spectra_fiducial.sqlite") as db:
 
 	#Process all the five power spectrum bins
 	for n in range(5):
 
 		#Name the feature
 		feature_name = "power_cross_{0}{0}".format(n)
+		projected_feature_name = feature_name + "_projected"
 		specs[feature_name] = dict()
+		specs[projected_feature_name] = dict()
 
 		#Add the emulator
-		emulator = Emulator.read("../models/emulator_power_cross_{0}{0}.pkl".format(n))
+		emulator = Emulator.read("../data/emulators/emulator_power_cross_{0}{0}.pkl".format(n))
+		specs[feature_name]["emulator"] = emulator
+
+		#Projected version of the emulator
 		manifold = Manifold(emulator)
 		curves = manifold.draw_tangents()
 		v = (curves['Om'].iloc[1],curves['w'].iloc[1])
 
 		#Refeaturize the emulator
-		specs[feature_name]["emulator"] = emulator.refeaturize(lambda f:manifold.pca.transform(f).project(v,names=['vOm','vw']),method="apply_whole")
-		specs[feature_name]["emulator"].to_pickle("../models/emulator_power_cross_projected_{0}{0}.pkl".format(n)) 
-
+		emulator_projected = emulator.refeaturize(lambda f:manifold.pca.transform(f).project(v,names=['vOm','vw']),method="apply_whole")
+		specs[projected_feature_name]["emulator"] = emulator_projected.combine_features({projected_feature_name:[feature_name]}) 
 
 		#Get the data and compute the covariance matrix
 		pow = db.query("SELECT {0} FROM features_fiducial WHERE b1={1} AND b2={1}".format(",".join(multipole_names),n))
 		pow.add_name(feature_name)
-		pow = manifold.pca.transform(pow).project(v,names=['vOm','vw'])
-		pow.add_name(feature_name)
-
 		specs[feature_name]["data"] = pow.head(1600).mean()
 		specs[feature_name]["data_covariance"] = pow.cov() / 1600
 
+		#Do the same for the projected features
+		pow = manifold.pca.transform(pow).project(v,names=['vOm','vw'])
+		pow.add_name(projected_feature_name)
+		specs[projected_feature_name]["data"] = pow.head(1600).mean()
+		specs[projected_feature_name]["data_covariance"] = pow.cov() / 1600
+
 #Execute
-driver.chi2database(db_name="../models/power_cross_scores_projected_scaled.sqlite",parameters=score_parameters,specs=specs,pool=None,nchunks=None)
+driver.chi2database(db_name="../data/scores/power_cross_scores_LSST.sqlite",parameters=score_parameters,specs=specs,pool=None,nchunks=None)
