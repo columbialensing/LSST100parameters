0172f68949d50261fe61fc0626c6fb9eb5315f8b
apetri <apetri@phys.columbia.edu>
2015-11-04 09:49:53 -0800

    fixed typo

--- code/library/driver_mpi.py
+++ code/library/driver_mpi.py
@@ -21,14 +21,16 @@ def measure(batch,cosmo_id,catalog_names,model_n,db_name,table_names,measurer,po
 	#Populate database
 	db_full_name = os.path.join(batch.environment.storage,db_name)
 	with FeatureDatabase(db_full_name) as db:
-
-		print("[+] Populating table '{0}' of database {1}...".format(table_name,db_full_name))
 			
 		#Handle on the model 
 		model = batch.getModel(cosmo_id)
 			
 		#Process sub catalogs
 		for nc,catalog_name in enumerate(catalog_names):
+
+			#Log to user
+			print("[+] Populating table '{0}' of database {1}...".format(table_names[nc],db_full_name))
+
 			for s,sc in enumerate(model.getCollection("512b260").getCatalog(catalog_name).subcatalogs):
 				print("[+] Processing model {0}, catalog {1}, sub-catalog {2}...".format(model_n,catalog_name,s+1))
 				db.add_features(table_names[nc],sc,measurer=measurer,extra_columns={"model":model_n,"sub_catalog":s+1},pool=pool,**kwargs)
