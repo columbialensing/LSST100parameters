5567486d8e4cff120f6939f462711f04f84404bf
apetri <apetri@phys.columbia.edu>
2016-01-14 08:25:28 -0800

    measure peak counts from catalog

--- code/measure/measure_peaks.py
+++ code/measure/measure_peaks.py
@@ -29,7 +29,7 @@ def peaks(maps,kappa_edges,indices):
 		
 	#Measure the auto and cross power spectrum
 	for n,i in enumerate(indices):
-		kappa,peaks_array[n] = maps[i].peakCount(kappa_edges)
+		kappa,peaks_array[n] = maps[i].peakCount(kappa_edges,norm=True)
 
 	#Build the Ensemble
 	columns = [ "k{0}".format(n) for n in range(1,len(kappa)+1) ]
@@ -58,7 +58,7 @@ if __name__=="__main__":
 
 	#Redshift bin indices and kappa thresholds
 	indices = range(5)
-	kappa_edges = pd.read_pickle("/global/homes/a/apetri/LSST100Parameters/data/edges.pkl")["kappa_edges"].values
+	kappa_edges = pd.read_pickle("/global/homes/a/apetri/LSST100Parameters/data/edges.pkl")["kappa_edges_sigma"].values
 
 	#Execute
 	for model_id in cmd_args.id:
