000a19d4e01b7877f589f6b31ae6b7078123f6fe
apetri <apetri@phys.columbia.edu>
2016-01-27 15:14:30 -0500

    process a different number of PCA components

--- code/library/driver_mpi.py
+++ code/library/driver_mpi.py
@@ -145,28 +145,27 @@ def cosmo_constraints(batch,specs,settings=default_settings):
 		df.pop("realization")
 		df.add_name("features")
 
+	#Use the emulator to figure out the PCA components
+	print("[+] Computing principal components...")
+	pca = emulator[["features"]].principalComponents(scale=emulator[["features"]].mean())
+
+	#Transform the emulator,data and covariance with a PCA projection
+	print("[+] Projecting emulator on principal components...")
+	emulator = emulator.refeaturize(pca.transform,method="apply_whole")
+
+	print("[+] Projecting covariance ensemble on principal components...")
+	covariance = pca.transform(covariance)
+
+	print("[+] Date ensemble on principal components...")
+	data = pca.transform(data)
+
+	#add names to data and covariance to match labels
+	for df in [data,covariance]:
+		df.add_name("features")
+
 	#############
 	#Constraints#
 	#############
-
-	#Train the emulator with a multiquadric kernel
-	print("[+] Training emulator with multiquadric kernel...")
-	emulator.train(method=multiquadric)
-
-	#Approximate the emulator linearly around the fiducial parameters to get a FisherAnalysis instance
-	print("[+] Approximating the emulator linearly around ({0})=({1}), derivative precision={2:.2f}...".format(",".join(pnames),",".join(["{0:.2f}".format(p) for p in settings.fiducial_parameters]),settings.derivative_precision))
-	fisher = emulator.approximate_linear(settings.fiducial_parameters,settings.derivative_precision)
-
-	#Compute the parameter covariance matrix correcting for the inverse covariance bias
-	print("[+] Computing the {0}x{0} feature covariance matrix, Nreal={1}...".format(covariance.shape[1],specs["realizations_for_covariance"]))
-	feature_covariance = covariance.head(specs["realizations_for_covariance"]).cov()
-
-	print("[+] Computing {0}x{0} parameter covariance matrix, Nbins={1}...".format(len(pnames),covariance.shape[1]))
-	parameter_covariance = fisher.parameter_covariance(feature_covariance,correct=specs["realizations_for_covariance"])
-
-	################################
-	#Output to constraints database#
-	################################
 
 	#Column names
 	pcov_columns = ["{0}-{1}".format(i,j) for i,j in itertools.product(pnames,pnames)]
@@ -175,14 +174,49 @@ def cosmo_constraints(batch,specs,settings=default_settings):
 	outdbname = os.path.join(batch.home,"data",specs["dbname"])
 	print("[+] Saving constraints to {0}, table '{1}'".format(outdbname,specs["table_name"]))
 
-	#Format the row to insert
-	row = pd.Series(parameter_covariance.values.flatten(),index=pcov_columns)
-	row["bins"] = covariance.shape[1]
-	row["feature_label"] = specs["feature_label_root"]
-
-	#Insert the row
 	with FeatureDatabase(outdbname) as db:
-		db.insert(pd.DataFrame(row).T,specs["table_name"])
+
+		for nc in specs["pca_components"]:
+
+			#Build feature label that enters in database
+			feature_label = specs["feature_label_format"].format(specs["feature_label_root"],nc,specs["realizations_for_covariance"],specs["realizations_for_data"])
+			print("[+] Processing {0} PCA components; feature label '{1}'".format(nc,feature_label))
+
+			#Build names of column subset
+			columns_nc = [("features",n) for n in range(nc)]
+			emulator_nc = [("parameters",p) for p in pnames] + columns_nc
+
+			#Truncate emulator,covariance and data to the appropriate number of principal components
+			emulator_pca = emulator[emulator_nc].copy()
+			covariance_pca = covariance[columns_nc]
+			data_pca = data[columns_nc]
+
+			#Train the emulator with a multiquadric kernel
+			print("[+] Training emulator with multiquadric kernel...")
+			emulator_pca.train(method=multiquadric)
+
+			#Approximate the emulator linearly around the fiducial parameters to get a FisherAnalysis instance
+			print("[+] Approximating the emulator linearly around ({0})=({1}), derivative precision={2:.2f}...".format(",".join(pnames),",".join(["{0:.2f}".format(p) for p in settings.fiducial_parameters]),settings.derivative_precision))
+			fisher = emulator_pca.approximate_linear(settings.fiducial_parameters,settings.derivative_precision)
+
+			#Compute the parameter covariance matrix correcting for the inverse covariance bias
+			print("[+] Computing the {0}x{0} feature covariance matrix, Nreal={1}...".format(covariance_pca.shape[1],specs["realizations_for_covariance"]))
+			feature_covariance = covariance_pca.head(specs["realizations_for_covariance"]).cov()
+
+			print("[+] Computing {0}x{0} parameter covariance matrix, Nbins={1}...".format(len(pnames),covariance_pca.shape[1]))
+			parameter_covariance = fisher.parameter_covariance(feature_covariance,correct=specs["realizations_for_covariance"])
+
+			################################
+			#Output to constraints database#
+			################################
+
+			#Format the row to insert
+			row = pd.Series(parameter_covariance.values.flatten(),index=pcov_columns)
+			row["bins"] = covariance_pca.shape[1]
+			row["feature_label"] = feature_label
+
+			#Insert the row
+			db.insert(pd.DataFrame(row).T,specs["table_name"])
 
 
 
