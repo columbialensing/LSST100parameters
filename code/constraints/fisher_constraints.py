257117a4b0cd43056fa1588019cc6ee913c1dc1b
apetri <apetri@phys.columbia.edu>
2016-01-28 07:42:26 -0800

    added verbosity in cmd_args

--- code/constraints/fisher_constraints.py
+++ code/constraints/fisher_constraints.py
@@ -5,13 +5,15 @@ sys.modules["mpi4py"] = None
 
 import argparse,json
 
-from lenstools import SimulationBatch
+from library.featureDB import LSSTSimulationBatch
 from library.driver_mpi import cosmo_constraints
+from library.defaults import settings
 
 #Command line options
 parser = argparse.ArgumentParser()
 parser.add_argument("-e","--environment",dest="environment",action="store",default="environment.ini",help="INI file with the batch location")
 parser.add_argument("-f","--features",dest="features",action="store",default="combine_default.json",help="JSON file containing the specifications of the features to combine")
+parser.add_argument("-v","--verbose",dest="verbose",action="store_true",default=False,help="Turn on verbosity")
 
 #Main
 def main():
@@ -20,7 +22,7 @@ def main():
 	cmd_args = parser.parse_args()
 
 	#Init batch
-	batch = SimulationBatch.current(cmd_args.environment)
+	batch = LSSTSimulationBatch.current(cmd_args.environment)
 
 	#Read json specs and sanitize input
 	with open(cmd_args.features,"r") as fp:
@@ -32,7 +34,7 @@ def main():
 				specs[feature][l] = None
 		
 	#Execute
-	cosmo_constraints(batch,specs)
+	cosmo_constraints(batch,specs,settings,cmd_args.verbose)
 
 
 if __name__=="__main__":
