0d2091b0473969b0e8ef02f2f2aa5c8ec50ce262
apetri <apetri@phys.columbia.edu>
2016-01-28 15:01:33 -0500

    direct stdout and stderr to files

--- containers/0-ngenic.sh
+++ containers/0-ngenic.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mpiexec -n 32 /N-body/NGenIC/NGenIC /TestRun/models/Om0.260_Ol0.740_w-1.000_si0.800/512b260/ic1/ngenic.param
+mpiexec -n 32 /N-body/NGenIC/NGenIC /TestRun/models/Om0.260_Ol0.740_w-1.000_si0.800/512b260/ic1/ngenic.param > ngenic.out 2> ngenic.err
--- containers/2-planes.sh
+++ containers/2-planes.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mpiexec -n 16 lenstools.planes-mpi -e /TestRun/environment.ini -c /TestRun/planes.ini 'Om0.260_Ol0.740_w-1.000_si0.800|512b260|ic1'
+mpiexec -n 16 lenstools.planes-mpi -e /TestRun/environment.ini -c /TestRun/planes.ini -v 'Om0.260_Ol0.740_w-1.000_si0.800|512b260|ic1' > planes.out 2> planes.err
