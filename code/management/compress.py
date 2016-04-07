7a74ae51dc1085545bd95039beacd60d26e886d5
apetri <apetri@phys.columbia.edu>
2016-04-07 12:10:57 -0400

    more flexibility in compress/unpack scripts to handle planes/catalogs/snapshots, etc...

--- code/management/compress.py
+++ code/management/compress.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python-mpi
 import os
+import argparse
 
 from lenstools.utils import MPIWhirlPool
 from lenstools.pipeline.simulation import SimulationBatch
 
 #Get the resource to compress
-def shear(model,batch):
-	full_path =  model.collections[0].getCatalog("Shear").storage_subdir
+def shear(model,batch,search_string):
+	full_path =  model[search_string].storage
 	rel_path = os.path.relpath(full_path,batch.environment.storage)
 	return rel_path
 
@@ -16,12 +17,20 @@ try:
 except:
 	pool = None
 
-#Current simulation batch 
-batch = SimulationBatch.current()
-models = batch.models
-archive_names = ["archive/{0}.tar.gz".format(m.cosmo_id) for m in models]
+#Parse command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-e","--environment",dest="env_file",action="store",type=str,default="environment.ini",help="environment option file")
+parser.add_argument("-a","--archive",dest="archive",action="store",type=str,default="archive/{0}.tar.gz",help="archive name format")
+parser.add_argument("-s","--search",dest="search",action="store",type=str,default="c0C0",help="search string that when dialed points to the resource")
 
-batch.archive(archive_names,pool=pool,resource=shear,chunk_size=1,batch=batch)
+cmd_args = parser.parse_args()
+
+#Current simulation batch 
+batch = SimulationBatch.current(cmd_args.env_file)
+models = batch.models
+archive_names = [cmd_args.archive.format(m.cosmo_id) for m in models]
+
+batch.archive(archive_names,pool=pool,resource=shear,chunk_size=1,batch=batch,search_string=cmd_args.search)
 
 
 pool.comm.Barrier()
